855631be2ba81f279b36b73ef6098a84744ae1f4
Cristian Mesa Sanchez <cristian.mesa.sanchez@estudiantat.upc.edu>
2022-12-17 13:36:03 +0100

    Added indicators and fixed colors

--- healthindicators/healthIndicatorsUtils.py
+++ healthindicators/healthIndicatorsUtils.py
@@ -4,6 +4,9 @@ MILLIGRAMS = "mg"
 MICROLITER = "μL"
 MICROLITERPERMINUTE = "μL/min"
 
+# Minimum indicators to have a unhealthy heart
+MINIMUM_INDICATORS_UNHEALTHY_HEART = 3
+
 # Intervals for compareHealthIndicators #
 
 # BW Intervals
@@ -66,32 +69,47 @@ SV_NEGATIVEINTERVAL = 35.1-8.5
 CO_POSITIVEINTERVAL = 17.7+3.8
 CO_NEGATIVEINTERVAL = 17.7-3.8
 
+
+# PRINT UTILS
+NORMAL_TEXT = "\033[0m"
+BOLD_TEXT = "\033[1m"
+RED_TEXT = "\x1b[1;31m"
+GREEN_TEXT = "\x1b[1;32m"
+
+
+
 def checkInterval(negativeInterval, positiveInterval, value, name):
     negativeInterval = round(negativeInterval, 2)
     positiveInterval = round(positiveInterval, 2)
     healthy = True
-    pr = str(name)+" value and interval : "
+    pr = BOLD_TEXT + str(name) + NORMAL_TEXT + " value and interval : "
 
     if value < negativeInterval:
-        pr = pr+"\x1b[1;31m"+str(value)+'\033[0;m'
+        pr = pr+RED_TEXT+str(value)+NORMAL_TEXT
         healthy = False
 
     pr = pr + " " + str(negativeInterval)
     
     if value >= negativeInterval and value <= positiveInterval:
-        pr = pr + " "+ ("\x1b[1;32m"+str(value)+'\033[0;m')
+        pr = pr + " "+ (GREEN_TEXT+str(value)+NORMAL_TEXT)
 
     pr = pr + " " + str(positiveInterval)
 
     if value > positiveInterval:
-        pr = pr + " "+ ("\x1b[1;31m"+str(value)+'\033[0;m')
+        pr = pr + " "+ (RED_TEXT+str(value)+NORMAL_TEXT)
         healthy = False
-
+    
     print(pr)
-    if(healthy): print("VALUE IS" + "\x1b[1;32m"+ " HEALTHY!"+'\033[0;m') 
-    else: print("VALUE IS" + "\x1b[1;31m"+ " UNHEALTHY!"+'\033[0;m') 
+
+    if(healthy): print("VALUE IS" + GREEN_TEXT+ " HEALTHY!"+NORMAL_TEXT+"\n") 
+    else: print("VALUE IS" + RED_TEXT+ " UNHEALTHY!"+NORMAL_TEXT+"\n") 
+    
 
     return healthy
+
+
+
+
 
 
 
