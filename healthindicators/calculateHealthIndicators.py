d1ab1a10a37c9a09d13f29a1a0003c80591a9376
Cristian Mesa Sanchez <cristian.mesa.sanchez@estudiantat.upc.edu>
2022-12-17 10:11:03 +0100

    Documentation of the functions done

---
+++ healthindicators/calculateHealthIndicators.py
@@ -0,0 +1,64 @@
+
+####### CALCULATIONS TO KNOW THE HEALTH INDICATORS #######
+
+# LVESV = [7/(2.4+LVIDs)]*(LVIDs^3)
+def calculateLVESV():
+    """
+    CALCULATE LVESV = [7/(2.4+LVIDs)]*(LVIDs^3)
+    """
+    return
+
+# LVEDV = [7/(2.4+LVIDd)]*(LVIDd^3)
+def calculateLVEDV():
+    """
+    CALCULATE LVEDV = [7/(2.4+LVIDd)]*(LVIDd^3)
+    """
+    return
+
+# FS(%) = (LVIDd-LVIDs)/LVIDd*100
+def calculateFS():
+    """
+    CALCULATE FS(%) = (LVIDd-LVIDs)/LVIDd*100
+    """
+    return
+
+# EF(%) = (LVEDV-LVESV)/LVEDV*100
+def calculateEF():
+    """
+    CALCULATE EF(%) = (LVEDV-LVESV)/LVEDV*100
+    """
+    return
+
+# LV mass (mg) = 1.04[(LVIDd+LVAWd+LVPWd)^3-(LVIDd^3)]*0.8+0.6
+def calculateLV_mass():
+    """
+    # CALCULATE LV mass (mg) = 1.04[(LVIDd+LVAWd+LVPWd)^3-(LVIDd^3)]*0.8+0.6
+
+    """
+    return
+
+# Stroke volume, SV (μL) = (LVEDV-LVESV)
+def calculateStroke_volume():
+    """
+    CALCULATE Stroke volume: SV (μL) = (LVEDV-LVESV)
+
+    """
+    return
+
+# Cardiac output : CO(μL/min) = SV*HR
+def calculateCardiac_output():
+    """
+    CALCULATE Cardiac output : CO(μL/min) = SV*HR
+
+    """
+    return
+
+# Relative wall thickness: RWT = (LVPWd+LVIVSd)/(LVIDd)
+def calculateRelative_wall_thickness():
+    """
+    Relative wall thickness: RWT = (LVPWd+LVIVSd)/(LVIDd)
+
+    """
+    return
+
+
---
+++ healthindicators/compareHealthIndicators.py
@@ -0,0 +1,163 @@
+
+####### COMPARATORS: 3 OR MORE = UNHEALTHY HEART ####### 
+
+# BW (g) 25.7+-3.6
+def compareBW():
+    """
+    Compare if BW (g) 25.7+-3.6
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+# HR(b.p.m) 535+-75
+def compareHR():
+    """
+    Compare if HR(b.p.m) 535+-75
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+# LV mass (mg) 96+-18
+def compareLV_mass():
+    """
+    Compare if LV mass (mg) 96+-18
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+#LVPWd (mm) 0.79+-0.22
+def compareLVPWd():
+    """
+    Compare if LVPWd (mm) 0.79+-0.22
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+#LVPWs (mm) 1.12+-0.33
+def compareLVPWs():
+    """
+    Compare if LVPWs (mm) 1.12+-0.33
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+#LVIDs (mm) 2.20+-0.50
+def compareLVIDs():
+    """
+    Compare if LVIDs (mm) 2.20+-0.50
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+#LVIDd (mm) 3.69+-0.41
+def compareLVIDd():
+    """
+    Compare if LVIDd (mm) 3.69+-0.41
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+#IVSd (mm) 0.71+-0.15
+def compareIVSd():
+    """
+    Compare if IVSd (mm) 0.71+-0.15
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+#IVSs (mm) 0.97+-0.19
+def compareIVSs():
+    """
+    Compare if IVSs (mm) 0.97+-0.19
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+#LVESV (μL) 19.35+-11.30
+def compareLVESV():
+    """
+    Compare if LVESV (μL) 19.35+-11.30
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+#LVEDV (μL) 57.7+-16.5
+def compareLVEDV():
+    """
+    Compare if LVEDV (μL) 57.7+-16.5
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+#EF(%) 71+-11
+def compareEF():
+    """
+    Compare if EF(%) 71+-11
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+#FS(%) 43+-9
+def comparefs():
+    """
+    Compare if FS(%) 43+-9
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+#SV(μL) 35.1+-8.5
+def compareSV():
+    """
+    Compare if SV(μL) 35.1+-8.5
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+#CO(mL/min) 17.7+-3.8
+def compareCO():
+    """
+    Compare if CO(mL/min) 17.7+-3.8
+
+    TRUE = Healthy (between the intervals) \n
+    FALSE = Unhealthy (not between the invervals)
+    """
+    return
+
+def compareAll():
+    """
+    Compare all the health indicators, if number of unhealthy indicators are 3 or more the heart is unhealthy.
+
+    Return: \n
+    TRUE = Healthy heart // FALSE = Unhealthy heart \n
+    A list with all the comparations done and if the intervals are healthy or unhealthy
+    """
+    return
+
