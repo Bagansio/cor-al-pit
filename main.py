35d6aa38fe705c7a2414ca1e1625cc6883d3ff21
Bagansio <29057684+Bagansio@users.noreply.github.com>
2022-12-17 04:34:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
+
+### function that returns true if ndim < 4 also tell some errors
+# example : If dataset is a video will have 4 dims and then ds.pixel_array will be a matrix
 def valid_imshow_data(data):
     data = np.asarray(data)
     if data.ndim == 2:
@@ -26,9 +29,10 @@ def valid_imshow_data(data):
         return False
 
 
+# map dicom folder it could be improved to obtain a list of files with path to automatize analisis
 def map_dicom():
     fpath = "DICOM"
-    for root, dirs, files in walk(fpath):
+    for root, dirs, files in os.walk(fpath):
         for file in files:
             full_path = root+"\\"+file
             print("<------------------------------------->")
@@ -38,12 +42,15 @@ def map_dicom():
             print(ds)
         #print(root, dirs, files)
 
+
+# from a dataset of 4dim (video) render in images the video as images
 def render_images(ds):
     for i, slice in enumerate(ds.pixel_array):
         plt.imshow(slice)
         plt.savefig(f'images\\slice_{i:03n}.png')
 
 
+#from a set of images render a .avi video
 def render_video_from_images():
     image_folder = 'images'
     video_name = 'video.avi'
@@ -59,6 +66,8 @@ def render_video_from_images():
     cv2.destroyAllWindows()
     video.release()
 
+
+# MAIN starts here
 
 path = "DICOM\\1003\\0W\DICOM OK\\2018-04-12-17-52-41.dcm"
 ds = dcmread(path)
